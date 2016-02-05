02068f23c198f7870909329bcc74cf63aef2e349
Leif Bladt <leif.bladt@gmx.de>
2016-02-06 00:19:07 +0100

    Add blink method to LED class

--- LED.cpp
+++ LED.cpp
@@ -7,11 +7,39 @@ LED::LED(const int pin) {
 }
 
 void LED::powerOn() {
+  Serial.println("power on");
+  
+  _mode = SHINE;
   digitalWrite(_pin, HIGH);
 }
 
 void LED::powerOff() {
+  Serial.println("power off");
+
+  _mode = SHINE;
   digitalWrite(_pin, LOW);
 }
 
+void LED::blink(unsigned int frequency) {
+  Serial.printf("blink [%d]\n", frequency);
+
+  _mode = BLINK;
+  _lastStart = millis();
+  _duration = 1000.0 / frequency / 2.0;
+  digitalWrite(_pin, HIGH);
+}
+
+void LED::loop() {
+
+
+  if (_mode == BLINK) {
+    if (millis() - _lastStart < _duration) {
+      digitalWrite(_pin, HIGH);
+    } else if (millis() - _lastStart < 2 * _duration) {
+      digitalWrite(_pin, LOW);
+    } else {
+      _lastStart = millis();
+    }
+  }
+}
 
--- LED.h
+++ LED.h
@@ -9,9 +9,17 @@ class LED {
 
     void powerOff();
 
-    // TODO Blink, Fade, ...
+    void blink(unsigned int frequency);
+
+    void loop();
+
+    // TODO Fade, ...
 
   private:
     int _pin;
+    enum LEDMode { SHINE, BLINK };
+    LEDMode _mode = SHINE; 
+    long _lastStart;
+    unsigned int _duration;
 };
 #endif
