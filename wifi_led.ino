a17703b0d4c39a5d88dd51de473ed5d28e053d35
Leif Bladt <leif.bladt@gmx.de>
2016-02-05 23:30:50 +0100

    Add OTA functionality

--- wifi_led.ino
+++ wifi_led.ino
@@ -1,5 +1,9 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
 #include <PubSubClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 #include "Button.h"
 #include "LED.h"
 #include "LEDStripe.h"
@@ -104,6 +108,25 @@ void setup() {
   client.setCallback(callback);
 
   // TODO Set initial state
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("End");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
 }
 
 void reconnect() {
@@ -148,4 +171,6 @@ void loop() {
   }
 
   client.loop();
+
+  ArduinoOTA.handle();
 }
